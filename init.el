2d9bd4cd58db0a1da0e3836b370516ba57e2bc0c
daniel <daniel.phan36@gmail.com>
2016-12-20 17:26:14 -0800

    added multiple cursors previous match

--- init.el
+++ init.el
@@ -77,7 +77,8 @@
 (use-package multiple-cursors
   :ensure t
   :bind
-  (("C-c m n" . mc/mark-next-like-this)
+  (("C-c m p" . mc/mark-previous-like-this)
+   ("C-c m n" . mc/mark-next-like-this)
    ("C-c m l" . mc/edit-lines)
    ("C-c m a" . mc/mark-all-like-this)))
 
