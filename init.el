57edf7164d276b11e4aacc51406b6d9e1bcdefeb
mamapanda <daniel.phan36@gmail.com>
2016-12-27 14:15:58 -0800

    removed unnecessary flycheck and company +1 in setup-tide-mode

--- init.el
+++ init.el
@@ -181,10 +181,8 @@
   (defun setup-tide-mode ()
     (interactive)
     (tide-setup)
-    (flycheck-mode +1)
     (eldoc-mode +1)
-    (tide-hl-identifier-mode +1)
-    (company-mode +1))
+    (tide-hl-identifier-mode +1))
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide-mode))
 
