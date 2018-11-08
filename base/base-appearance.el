287b26834cdd6604e632b930af8ce890dddfbe1c
panda <daniel.phan36@gmail.com>
2018-11-07 22:40:37 -0800

    started transition to org-mode

--- base/base-appearance.el
+++ base/base-appearance.el
@@ -5,24 +5,9 @@
 
 ;;; Code:
 
-(use-package nlinum
-  :bind (("C-c n" . nlinum-mode)))
 
-(use-package nyan-mode
-  :config
-  (nyan-mode t))
 
-(use-package rainbow-delimiters
-  :init
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
-(use-package spaceline-config
-  :ensure spaceline
-  :config
-  (spaceline-emacs-theme)
-  (spaceline-toggle-buffer-size-off)
-  (spaceline-toggle-buffer-modified-off)
-  (spaceline-toggle-hud-off))
 
 (provide 'base-appearance)
 ;;; base-appearance.el ends here
