f0e3443b11b2ad5954c29d96b6566a3e943fac63
panda <daniel.phan36@gmail.com>
2018-11-08 16:46:48 -0800

    bug fixes

--- custom-file.el
+++ custom-file.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (tide typescript-mode flycheck-rust racer rust-mode company-anaconda anaconda-mode powershell markdown-mode company-tern tern js2-mode web-mode intero haskell-mode company-go go-eldoc go-mode slime clojure-mode omnisharp csharp-mode flycheck-irony company-irony-c-headers company-irony irony realgud git-timemachine magit projectile neotree ace-window counsel yasnippet undo-tree origami multiple-cursors flycheck expand-region corral company anzu pacmacs hydra flx fireplace esup rainbow-delimiters nyan-mode nlinum diminish spaceline spacemacs-theme monokai-theme use-package))))
 ;; hide toolbar
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
