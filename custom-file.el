bc95b87cd8e4dcebadd14dc0dd2b68b0c4db8559
panda <daniel.phan36@gmail.com>
2018-11-10 14:18:36 -0800

    added which-key and beacon

--- custom-file.el
+++ custom-file.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (smex tide typescript-mode flycheck-rust racer rust-mode company-anaconda anaconda-mode powershell markdown-mode company-tern tern js2-mode web-mode intero haskell-mode company-go go-eldoc go-mode slime clojure-mode omnisharp csharp-mode flycheck-irony company-irony-c-headers company-irony irony realgud git-timemachine magit projectile neotree ace-window counsel yasnippet undo-tree origami multiple-cursors flycheck expand-region corral company anzu pacmacs hydra flx fireplace esup rainbow-delimiters nyan-mode nlinum diminish spaceline spacemacs-theme monokai-theme use-package))))
+    (which-key beacon smex tide typescript-mode flycheck-rust racer rust-mode company-anaconda anaconda-mode powershell markdown-mode company-tern tern js2-mode web-mode intero haskell-mode company-go go-eldoc go-mode slime clojure-mode omnisharp csharp-mode flycheck-irony company-irony-c-headers company-irony irony realgud git-timemachine magit projectile neotree ace-window counsel yasnippet undo-tree origami multiple-cursors flycheck expand-region corral company anzu pacmacs hydra flx fireplace esup rainbow-delimiters nyan-mode nlinum diminish spaceline spacemacs-theme monokai-theme use-package))))
 ;; hide toolbar
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
