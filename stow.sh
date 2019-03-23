ea9092c0c3a1a851813bd69ce67d76e6fa885085
panda <daniel.phan36@gmail.com>
2019-03-23 15:12:59 -0700

    added i3

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-stowdirs=("alacritty" "bash" "emacs" "lang" "nvim")
+stowdirs=("alacritty" "bash" "emacs" "i3" "lang" "nvim" "x")
 
 for stowdir in ${stowdirs[*]}; do
     stow $stowdir
