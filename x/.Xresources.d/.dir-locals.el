caa5c217559feceb59e3bea99fbb41b229190cd6
panda <daniel.phan36@gmail.com>
2019-06-09 22:04:51 -0700

    restructure Xresources

--- x/.Xresources.d/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c-mode
-  (eval panda-format-on-save-mode)
-  (eval clang-format-on-save-mode -1)))
