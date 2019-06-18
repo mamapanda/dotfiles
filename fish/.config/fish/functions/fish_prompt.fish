923a86d6780c6e274d31e9a685c347f684d696aa
panda <daniel.phan36@gmail.com>
2019-06-18 14:56:49 -0700

    change theme to doom-vibrant, add doom-vibrant & flat-remix blue darkest

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 function fish_prompt
-    set_color --bold green
+    set_color --bold blue
     echo -n (printf "[%s@%s " $USER (prompt_hostname))
 
     set_color --bold $fish_color_cwd
     echo -n (basename (prompt_pwd))
 
-    set_color --bold green
+    set_color --bold blue
     echo -n "]\$ "
 
     set_color normal
