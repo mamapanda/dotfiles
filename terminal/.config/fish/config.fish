d81e85ea7c8f268428b6e1d6c935f88b2602aab3
panda <daniel.phan36@gmail.com>
2019-11-04 18:40:43 -0800

    fish: use config.fish instead of fish_variables

---
+++ terminal/.config/fish/config.fish
@@ -0,0 +1,37 @@
+# https://github.com/fish-shell/fish-shell/issues/4542
+for v in (set --show | string replace -rf '^\$([^:[]+).*: set in universal.*' '$1')
+    set -e $v
+end
+
+# disable annoying startup text
+set -g __fish_init_2_3_0
+set -g fish_greeting
+
+# vi keys
+fish_vi_key_bindings
+set -g fish_escape_delay_ms 10
+
+# syntax colors
+set -g fish_color_command magenta
+set -g fish_color_param white
+set -g fish_color_valid_path --underline
+set -g fish_color_quote green  # strings
+set -g fish_color_escape cyan
+set -g fish_color_operator yellow
+set -g fish_color_end yellow  # process separators (e.g. | and &)
+set -g fish_color_redirection yellow
+set -g fish_color_cancel red  # ^C
+set -g fish_color_comment white
+set -g fish_color_error red
+
+# completion colors
+set -g fish_color_autosuggestion blue  # untyped part of suggestion
+set -g fish_color_search_match bryellow --background=brblack  # pager selection
+set -g fish_pager_color_prefix yellow  # typed parts of completions
+set -g fish_pager_color_completion normal  # untyped parts of completions
+set -g fish_pager_color_description yellow
+set -g fish_pager_color_progress yellow
+
+# other colors
+set -g fish_color_history_current yellow  # dirh current directory
+set -g fish_color_selection white --bold --background=black  # vi selection
