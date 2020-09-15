cd487589525275446a99d8a4a677f0b08a675acb
panda <daniel.phan36@gmail.com>
2020-09-14 22:11:56 -0700

    Update browser settings

--- .unused/qutebrowser/.config/qutebrowser/config.py
+++ .unused/qutebrowser/.config/qutebrowser/config.py
@@ -10,11 +10,11 @@ config.bind(";", "set-cmd-text :")
 config.unbind(":")
 for binding, command in c.bindings.default["normal"].items():
     if binding[0] == ";":
-        config.bind(":" + binding[1:], command)
+        config.bind("," + binding[1:], command)
 
 c.editor.command = ["emacs", "--no-desktop", "{file}"]
 
-c.fonts.hints = "11pt monospace"
+c.fonts.hints = "11pt consolas"
 c.fonts.monospace = "consolas"
 
 c.hints.chars = "hjklasdfgyuiopqwertnmzxcvb"
@@ -26,8 +26,11 @@ c.tabs.last_close = "close"
 c.url.default_page = "https://www.startpage.com"
 c.url.searchengines = {
     "DEFAULT": "https://www.startpage.com/do/dsearch?query={}",
+    "amazon": "https://www.amazon.com/s?k={}",
+    "goo": "https://dictionary.goo.ne.jp/srch/all/{}/m0u/",
     "google": "https://www.google.com/search?q={}",
     "jisho": "https://jisho.org/search/{}",
+    "weblio": "https://www.weblio.jp/content/{}",
     "youtube": "https://www.youtube.com/results?search_query={}",
 }
 c.url.start_pages = ["https://www.startpage.com"]
