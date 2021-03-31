4dacfe8ee1d20b8ad5905abac634c6ffa24f85ef
Daniel Phan <daniel.phan36@gmail.com>
2021-03-30 23:30:45 -0700

    Update Emacs config
    Significant changes:
    
    - Can use <C-l> in evil-ex to select a line with Avy and insert the
    corresponding line number
    - Add C#
    - Add minimal init file for testing

---
+++ editor/.emacs.d/test-init.el
@@ -0,0 +1,19 @@
+(require 'package)
+
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("gnu" . 1)
+        ("melpa" . 10)))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t
+      use-package-always-demand t)
